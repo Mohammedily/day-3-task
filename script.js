29ca61af4d54ea1a58da81b20b971be3a62efbc1
mohammed ilyas <MY_NAME@example.com>
2021-11-11 19:19:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+ var resume = [{
+"basics": {
+    "name": "K.Mohammed ilyas",
+    "label": "Programmer",
+    "email": "m9741305@gmail.com",
+    "phone": 7845488778,
+    "location": {
+      "address": "8/28 mkhzbxbbx",
+      "pinCode": 098777,
+      "city": "88f8y8yr8",
+    },
+}
+ [{   "education": {
+     "college name": "st.xavier's polythechnic college ",
+     "Department":"diplamo mechanical engneering",
+     "percentage": "65%",
+ }   
+}],
+}]
+
+// var output = document.getElementById('output');
+// output.innerHTML = resume;
+
+console.log(resume);
